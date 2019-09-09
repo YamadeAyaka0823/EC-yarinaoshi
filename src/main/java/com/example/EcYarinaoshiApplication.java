6e59416f891ff343b0928a361d79c749dca655da
yamadeayaka <ayaka.yamade@rakus-partners.co.jp>
2019-09-09 12:22:23 +0900

    aaa

---
+++ src/main/java/com/example/EcYarinaoshiApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcYarinaoshiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcYarinaoshiApplication.class, args);
+	}
+
+}
